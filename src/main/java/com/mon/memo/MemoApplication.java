ac833276206e1d30a17510a3140e4e09f3eb53f4
monique <enjuk0507@gmail.com>
2022-05-25 13:38:26 +0900

    initial setting

--- src/main/java/com/mon/memo/MemoApplication.java
+++ src/main/java/com/mon/memo/MemoApplication.java
@@ -1,13 +1,23 @@
 package com.mon.memo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
+@MapperScan(basePackages = "com.mon.memo.mapper")
 public class MemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemoApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
